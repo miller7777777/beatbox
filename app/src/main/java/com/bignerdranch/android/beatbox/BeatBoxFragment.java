1672e1ad0479ee0c026ea05f9d54df1c98eb905c
miller777 <miller777@mail.ru>
2018-04-03 00:20:11 +0300

    Поправил импорт;

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.bignerdranch.android.beatbox.databinding.FragmentBeatBoxBinding;
+import com.bignerdranch.android.beatbox.databinding.ListItemSoundBinding;
 
 public class BeatBoxFragment extends Fragment{
 
