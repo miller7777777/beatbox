034aa518ea4e493981189c03ecde71ceeb7bc185
Miller7777777 <miller777@mail.ru>
2018-04-13 18:25:41 +0300

    Retain Fragment;

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -28,6 +28,7 @@ public class BeatBoxFragment extends Fragment{
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
         mBeatbox = new BeatBox(getActivity());
     }
 
